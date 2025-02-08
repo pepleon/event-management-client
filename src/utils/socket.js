56cfef4ced7fa0e0fadb43699b564265153c2309
Aman Butt <156330345+pepleon@users.noreply.github.com>
2025-02-08 23:39:36 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,20 +2,13 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  const SERVER_URL = 
-    typeof window !== "undefined" && window.location.hostname === "localhost"
-      ? BASE_URL
-      : "https://event-management-backend-dzvv.onrender.com"; // Ensure it's correct
+if (location.hostname === "localhost"){
+    return io(BASE_URL)
+}
 
-  const token = localStorage.getItem("token"); // Retrieve JWT Token
+else {
+    return io("/", {path: "/api/socket.io"});
+}
+    
 
-  console.log("Connecting to:", SERVER_URL, "with token:", token);
-
-  return io(SERVER_URL, {
-    path: "/api/socket.io",
-    withCredentials: true,  // Ensures cookies are sent
-    auth: {
-      token: token,  // Send JWT in authentication
-    },
-  });
 };
